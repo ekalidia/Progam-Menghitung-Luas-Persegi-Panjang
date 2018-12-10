cf14891aeb99651c4f3183d89027ad049cf439c0
Eka Lidia Wati <45529774+ekalidia@users.noreply.github.com>
2018-12-10 16:06:53 +0700

    Add files via upload

---
+++ persegipanjang.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int p , l , luas;
+    cout<< " masukan panjang persegi panjang : ";
+    cin>> p;
+    cout<<endl;
+
+    cout<<" masukan lebar persegi panjang : ";
+    cin>> l;
+    cout<<endl;
+
+    luas = p * l;
+    cout<<" luas persegi panjang adalah "<< luas;
+    cout<<endl;
+
+    return 0;
+
+}
